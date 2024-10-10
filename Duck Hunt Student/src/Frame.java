755c9696bab9218cd51c74c12511fe3686df7386
adamisbird1 <164260245+adamisbird1@users.noreply.github.com>
2024-10-09 20:56:33 -0700

    Update Oct9

--- Duck Hunt Student/src/Frame.java
+++ Duck Hunt Student/src/Frame.java
@@ -1,7 +1,9 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -15,12 +17,62 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener {
-	Duck d = new Duck();
+	int roundTimer = 15;
+	Font bigFont = new Font("Serif", Font.BOLD, 30);
+	Ghost ghost = new Ghost();//duck
+	Pink pink = new Pink();//dog
+	Background background = new Background();
+	int score;
+	long time;
+	public void init() {
+roundTimer = 15;
+score= 0;
+time = 30;
+ghost.setWidthHeight(100, 100);
+ghost.setVx(2);
+ghost.setVy(2);
+pink.setXY(100, 100);
+	
+	}
+	public void reset(){
+		
+	}
 	
 	public void paint(Graphics g) {
 		super.paintComponent(g);
-		d.paint(g);
+		time += 20;
+		if(time%1000 == 0) {
+			roundTimer -= 1;
+			if (roundTimer == 0) {
+				// what do i do ater a new round?
+			}
+			
+		}
+		g.setFont(bigFont);
+		pink.paint(g);
+		background.paint(g);
+		ghost.paint(g);
+		ghost.setWidthHeight(100, 100);
+		ghost.setVx(5);
+		ghost.setVy(5);
+		pink.paint(g);
+		g.drawString("Round Timer " + roundTimer,0,50);//paint background and need to scale it
+		g.drawString("Score " + score,0,100);
+		//ghost bouncing around
+		if (ghost.getX() == 0) {
+			ghost.setVx(-2);
+		}
+		if (ghost.getX() == -20) {
+			ghost.setVx(5);
+		}	
+		if (ghost.getX() == 1000) {
+			ghost.setVx(-5);
+		}	
+		if (ghost.getY() == 800) {
+			ghost.setVy(-5);
+		}	
 	}
+	
 	
 	public static void main(String[] arg) {
 		Frame f = new Frame();
@@ -28,7 +80,7 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 	
 	public Frame() {
 		JFrame f = new JFrame("Duck Hunt");
-		f.setSize(new Dimension(900, 600));
+		f.setSize(new Dimension(1000, 800));
 		f.setBackground(Color.blue);
 		f.add(this);
 		f.setResizable(false);
@@ -61,10 +113,26 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 	}
 
 	@Override
-	public void mousePressed(MouseEvent arg0) {
+	public void mousePressed(MouseEvent mouse) {
 		// TODO Auto-generated method stub
-	
+	Rectangle rMouse = new Rectangle(mouse.getX(), mouse.getY(), 50, 50);
+	Rectangle rMain = new Rectangle(
+			ghost.getX(), ghost.getY(), 
+			ghost.getWidth(), ghost.getHeight()
+			);
+	//collision
+	if(rMouse.intersects(rMain)) {
+		ghost.setX((int)(Math.random() * 900) + 0);
+		ghost.setY((int)(Math.random() * 600) + 0);
+
+score += 1;
+
+//successful click//dog needs to move in the same -x pos
+pink.setX(ghost.getX());// may need to center
+pink.setY(300);
 	}
+	}
+	//score 
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
@@ -97,3 +165,6 @@ public class Frame extends JPanel implements ActionListener, MouseListener, KeyL
 	}
 
 }
+
+
+
