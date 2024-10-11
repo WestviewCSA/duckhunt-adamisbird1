e6f340344bac3c1c0cbde77ad2c1071bc27191a2
adamisbird1 <164260245+adamisbird1@users.noreply.github.com>
2024-10-11 08:10:21 -0700

    updated oct11

--- Duck Hunt Student/src/Frame.java
+++ Duck Hunt Student/src/Frame.java
@@ -10,72 +10,90 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
-
+//import jdk.internal.org.jline.utils.Timeout;
 public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener {
-	int roundTimer = 15;
+	int roundTimer;
 	Font bigFont = new Font("Serif", Font.BOLD, 30);
 	Ghost ghost = new Ghost();//duck
 	Pink pink = new Pink();//dog
+	Black black = new Black();
 	Background background = new Background();
 	int score;
 	long time;
-	public void init() {
-roundTimer = 15;
-score= 0;
-time = 30;
-ghost.setWidthHeight(100, 100);
-ghost.setVx(2);
-ghost.setVy(2);
-pink.setXY(100, 100);
-	
-	}
+	int boundX = 1;
+	int boundY = 1;
+	int on;
+
+	public void init() {//should initilize everthing
+		roundTimer = 10;
+		score= 0;
+		time = 1000;
+		ghost.setWidthHeight(100, 100);
+		ghost.setVx(5);
+		ghost.setVy(5);
+		ghost.setX((int)(Math.random() * 900) + 0);
+		ghost.setY((int)(Math.random() * 600) + 0);
+			
+			}
 	public void reset(){
 		
 	}
 	
 	public void paint(Graphics g) {
-		super.paintComponent(g);
+		super.paintComponent(g);	
+		ghost.setVx(8*boundX);
+		ghost.setVy(8*boundY);
 		time += 20;
 		if(time%1000 == 0) {
 			roundTimer -= 1;
 			if (roundTimer == 0) {
-				// what do i do ater a new round?
+				// what do i do after a new round?
 			}
-			
+		
 		}
+		//painting
+		if(roundTimer >= 0) {
 		g.setFont(bigFont);
 		pink.paint(g);
 		background.paint(g);
 		ghost.paint(g);
-		ghost.setWidthHeight(100, 100);
-		ghost.setVx(5);
-		ghost.setVy(5);
 		pink.paint(g);
 		g.drawString("Round Timer " + roundTimer,0,50);//paint background and need to scale it
 		g.drawString("Score " + score,0,100);
-		//ghost bouncing around
-		if (ghost.getX() == 0) {
-			ghost.setVx(-2);
-		}
-		if (ghost.getX() == -20) {
-			ghost.setVx(5);
-		}	
-		if (ghost.getX() == 1000) {
-			ghost.setVx(-5);
-		}	
-		if (ghost.getY() == 800) {
-			ghost.setVy(-5);
-		}	
-	}
+		on = 0;}
+		if(roundTimer <= 0) {
+			g.setFont(bigFont);
+			black.paint(g);
+			String end = "Your score was " + score + " Hit Space To Play Again";
+			g.drawString(end,200,300);
+			on = 1;}
+// add method for space to reset the score aand gametime back to 0
+		
+		
 	
+		//ghost bouncing around
+		if (ghost.getX() <= 0) {
+			boundX = boundX *-1;
+			}
+		if (ghost.getX() >= 900) {
+			boundX = boundX *-1;
+		}	
+		if (ghost.getY() <= 0) {
+			boundY = boundY *-1;
+		}	
+		if (ghost.getY() >= 650) {
+			boundY = boundY *-1;
+		}	
+		
+	}
 	
 	public static void main(String[] arg) {
 		Frame f = new Frame();
+		f.init();
 	}
 	
 	public Frame() {
@@ -99,72 +117,68 @@ pink.setXY(100, 100);
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void mouseExited(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void mousePressed(MouseEvent mouse) {
 		// TODO Auto-generated method stub
 	Rectangle rMouse = new Rectangle(mouse.getX(), mouse.getY(), 50, 50);
 	Rectangle rMain = new Rectangle(
-			ghost.getX(), ghost.getY(), 
+			ghost.getX(), ghost.getY(),
 			ghost.getWidth(), ghost.getHeight()
 			);
 	//collision
 	if(rMouse.intersects(rMain)) {
+		
+		
 		ghost.setX((int)(Math.random() * 900) + 0);
 		ghost.setY((int)(Math.random() * 600) + 0);
-
-score += 1;
-
+			boundX = boundX * -1;
+			boundY = boundY * -1;
+			score += 1;
 //successful click//dog needs to move in the same -x pos
 pink.setX(ghost.getX());// may need to center
-pink.setY(300);
+pink.setY(700);
 	}
 	}
-	//score 
-
+	//score
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		repaint();
 	}
-
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		System.out.println(arg0.getKeyCode());
+				if (arg0.getKeyCode()==32 && on == 1) {
+					score = 0;
+					roundTimer = 15;
+					
+				};
 	}
-
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
-
 }
-
 
 
